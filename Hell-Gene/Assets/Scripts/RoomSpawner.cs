0242bb836b9cd0c83e3685a90c798cbebb02fe2e
Benedict Wong <bwa44@sfu.ca>
2021-08-23 18:35:38 -0700

    added in generation for spawns

--- Hell-Gene/Assets/Scripts/RoomSpawner.cs
+++ Hell-Gene/Assets/Scripts/RoomSpawner.cs
@@ -9,23 +9,43 @@ public class RoomSpawner : MonoBehaviour
     // 2 - needs right door
     // 3 - needs top door
     // 4 - needs left door
+    private RoomTemplates templates;
+    private int rando;
+    public bool spawned = false;
+    void Start() {
+	    templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+	    Invoke("SpawnRooms", 3f);
+    }
+    
+    void SpawnRooms() {
+	    if (spawned == false) {
+		    if (openDirection == 1) {
+			    rando = Random.Range(0, 1);
+			    Instantiate(templates.downRooms[rando], transform.position, Quaternion.identity);
+		    
+		    }
+		    else if (openDirection == 2) {
+			    rando = Random.Range(0, 1);
+			    Instantiate(templates.rightRooms[rando], transform.position, Quaternion.identity);
+		    
+		    }
+		    else if (openDirection == 3) {
+			    rando = Random.Range(0, 1);
+			    Instantiate(templates.upRooms[rando], transform.position, Quaternion.identity);
+		    }
+		    else if (openDirection == 4) {
+			    rando = Random.Range(0, 1);
+			    Instantiate(templates.leftRooms[rando], transform.position, Quaternion.identity);
+		    }
 
-    void Update()
-    {
-        switch (openDirection)
-        {
-            case 1:
-                //spawn a room with BOTTOM door
-                break;
-            case 2:
-                //spawn a room with RIGHT door
-                break;
-            case 3:
-                //spawn a room with TOP door
-                break;
-            case 4:
-                //spawn a room with LEFT door
-                break;
-        }
+		    spawned = true;
+	    }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+	    if (collider.CompareTag("Spawnpoint")) {
+		    Destroy(gameObject);
+	    }
     }
 }
---
+++ Hell-Gene/Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+	public GameObject[] downRooms;
+	public GameObject[] upRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+	
+}
