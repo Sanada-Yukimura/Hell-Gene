2ede53de965bfffc6451e806cb59d16187e3813c
Kwiper <jonny.tibboh@gmail.com>
2021-08-24 17:21:59 -0700

    Basic boss AI and pattern setup

--- Hell-Gene/Assets/Scripts/Boss.cs
+++ Hell-Gene/Assets/Scripts/Boss.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Boss : MonoBehaviour
 {
@@ -17,19 +18,32 @@ public class Boss : MonoBehaviour
     public Transform firePoint;
 
     public Transform player;
+    public Pathfinding.AIPath aipath;
 
     Rigidbody2D rb;
 
     public bool isFlipped;
     public bool isAttacking;
 
+    public float maxChargeTime = 0.8f; // Telegraph boss attack
+    public float maxAttackTime = 0.3f; // Attack animation
+    public float maxCooldownTime = 1.0f; // Pause
+    public float maxAttackBuffer = 10f; // Buffer before boss is able to attack again
+    private float chargeTime;
+    private float attackTime;
+    private float cooldownTime;
+    public float attackBuffer;
+
     // Start is called before the first frame update
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
         phase = 0;
 
+        chargeTime = maxChargeTime;
+        attackTime = maxAttackTime;
+        cooldownTime = maxCooldownTime;
+        attackBuffer = maxAttackBuffer;
 
         isFlipped = false;
 
@@ -62,22 +76,58 @@ public class Boss : MonoBehaviour
 
         // Flip to player based on y
         LookAtPlayer();
-        
+
+        if (attackBuffer >= 0) {
+            attackBuffer -= Time.deltaTime;
+        }
+
+        if (isAttacking) {
+            aipath.canSearch = false;
+            aipath.canMove = false;
+
+            // Telegraph attack
+            if (chargeTime >= 0) {
+                chargeTime -= Time.deltaTime;
+                Debug.Log("Telegraphing Attack");
+            }
+
+            // Attack
+            if (chargeTime <= 0 && attackTime >= 0) {
+                attackTime -= Time.deltaTime;
+                Debug.Log("Attacking");
+            }
+
+            // Cooldown
+            if (attackTime <= 0 && cooldownTime >= 0) {
+                cooldownTime -= Time.deltaTime;
+                Debug.Log("Cooling Down");
+            }
+
+            // Return to move
+            if (cooldownTime <= 0) {
+                isAttacking = false;
+                chargeTime = maxChargeTime;
+                attackTime = maxAttackTime;
+                cooldownTime = maxCooldownTime;
+                attackBuffer = maxAttackBuffer;
+                aipath.canSearch = true;
+                aipath.canMove = true;
+            }
+
+        }
+
+
+
     }
 
-    private void FixedUpdate()
-    {
+    public void LookAtPlayer() {
 
-    }
-
-    void LookAtPlayer() {
-        if (transform.position.y > player.position.y && isFlipped)
+        if (transform.position.y > player.position.y && isFlipped && !isAttacking)
         {
             pivot.Rotate(0f, 0f, 180f);
             isFlipped = false;
-
         }
-        else if (transform.position.y < player.position.y && !isFlipped) {
+        else if (transform.position.y < player.position.y && !isFlipped && !isAttacking) {
             pivot.Rotate(0f, 0f, 180f);
             isFlipped = true;
         }
@@ -104,5 +154,19 @@ public class Boss : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (phase == 0 || phase == 2)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (!isAttacking && attackBuffer <= 0)
+                {
+                    isAttacking = true;
+                }
+            }
+        }
     }
 }
