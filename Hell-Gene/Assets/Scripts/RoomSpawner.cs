7ef9f827feec7674fea045553b981491a9fb46b9
Kybre <hermanchan10@gmail.com>
2021-08-25 08:31:15 -0700

    created enemy spawns for each module

--- Hell-Gene/Assets/Scripts/RoomSpawner.cs
+++ Hell-Gene/Assets/Scripts/RoomSpawner.cs
@@ -15,7 +15,7 @@ public class RoomSpawner : MonoBehaviour
 
     void Start() {
 	    templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-	    Invoke("SpawnRooms", 0.1f);
+	    Invoke("SpawnRooms", 0.5f);
     }
     
     void SpawnRooms() {
@@ -47,7 +47,6 @@ public class RoomSpawner : MonoBehaviour
             {
 	            Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
 	            Destroy(gameObject);
-
             }
             spawned = true;
 	    }
