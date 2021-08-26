e5acbbca82b772163d94a747b262e6d240b6367d
Benedict Wong <bwa44@sfu.ca>
2021-08-26 14:55:42 -0700

    added in scripts again

---
+++ Hell-Gene/Assets/Scripts/ChaosCardSelect.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChaosCardSelect : MonoBehaviour {
+	public Button card;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    card.onClick.AddListener(OnClicked);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClicked() {
+	    if (card.name == "RetainChaos") {
+		    PlayerPrefs.SetInt("currChaos", PlayerPrefs.GetInt("currChaos", 1));
+		    Debug.Log("Retain Chaos");
+	    }
+
+	    else if (card.name == "RaiseChaos") {
+		    PlayerPrefs.SetInt("currChaos", PlayerPrefs.GetInt("currChaos", 1)+1);
+		    Debug.Log("Raise Chaos");
+	    }
+    }
+}
